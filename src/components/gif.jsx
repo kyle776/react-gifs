a9dd8aedb88f00889c87f98faf03ee319ebb0036
Kyle Jobbins <kyle@jobbins.net>
2019-04-15 15:13:15 +0100

    Start new project from lewagon/react-boilerplate

---
+++ src/components/gif.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Gif extends Component {
+  render () {
+    return (
+      <div>
+        <img className="gif" src="https://media0.giphy.com/media/YPo1PBPh3A7SguqU8h/giphy.gif?cid=ecf05e475cb48f264259334b51d41dd0" alt=""/>
+      </div>
+    );
+  }
+};
+
+
+export default Gif;
---
+++ src/components/search_bar.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+  render () {
+    return (
+      <div>
+        <input type="text" className="form-search form-control"/>
+      </div>
+    );
+  }
+};
+
+
+export default SearchBar;
